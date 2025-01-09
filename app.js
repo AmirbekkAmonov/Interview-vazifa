d58ffd86d548000cfa84a3b70045082f2f60fba7
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-01-09 21:23:23 +0500

    .

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-// function topJuftliklar(array, maqsadQiymat) {
-//     const juftliklar = [];
-//     for (let i = 0; i < array.length; i++) {
-//       for (let j = i + 1; j < array.length; j++) {
-//         if (array[i] + array[j] === maqsadQiymat) {
-//           juftliklar.push([array[i], array[j]]);
-//         }
-//       }
-//     }
-//     return juftliklar;
-//   }
-//   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//   const maqsadQiymat = 10;
-//   console.log(topJuftliklar(array, maqsadQiymat)); 
+function topJuftliklar(array, maqsadQiymat) {
+    const juftliklar = [];
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[i] + array[j] === maqsadQiymat) {
+          juftliklar.push([array[i], array[j]]);
+        }
+      }
+    }
+    return juftliklar;
+  }
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const maqsadQiymat = 10;
+  console.log(topJuftliklar(array, maqsadQiymat)); 
 
 
 
@@ -19,52 +19,17 @@
 
 
 
-// function palindromTekshirish(soz) {
-//     soz = soz.toLowerCase();
-//     let teskariSoz = '';
-//     for (let i = soz.length - 1; i >= 0; i--) {
-//       teskariSoz += soz[i];
-//     }
-//     return soz === teskariSoz;
-//   }
-//   console.log(palindromTekshirish('level')); 
-//   console.log(palindromTekshirish('radar')); 
-//   console.log(palindromTekshirish('hello')); 
-
-
-
-
-
-
-
-
-
-// function anagramTekshirish(soz1, soz2) {
-//     if (soz1.length !== soz2.length) {
-//       return false;
-//     }
-//     const belgiHisobi = {};
-//     for (let i = 0; i < soz1.length; i++) {
-//       const belgi = soz1[i];
-//       belgiHisobi[belgi] = (belgiHisobi[belgi] || 0) + 1;
-//     }
-//     for (let i = 0; i < soz2.length; i++) {
-//       const belgi = soz2[i];
-//       if (!belgiHisobi[belgi]) {
-//         return false;
-//       }
-//       belgiHisobi[belgi]--;
-//     }
-//     for (const hisob in belgiHisobi) {
-//       if (belgiHisobi[hisob] !== 0) {
-//         return false;
-//       }
-//     }
-//     return true;
-//   }
-//   console.log(anagramTekshirish('listen', 'silent')); 
-//   console.log(anagramTekshirish('listen', 'silentx')); 
-//   console.log(anagramTekshirish('listen', 'silenty')); 
+function palindromTekshirish(soz) {
+    soz = soz.toLowerCase();
+    let teskariSoz = '';
+    for (let i = soz.length - 1; i >= 0; i--) {
+      teskariSoz += soz[i];
+    }
+    return soz === teskariSoz;
+  }
+  console.log(palindromTekshirish('level')); 
+  console.log(palindromTekshirish('radar')); 
+  console.log(palindromTekshirish('hello')); 
 
 
 
@@ -74,21 +39,56 @@
 
 
 
-// function fibonacci(n) {
-//     if (n === 0) {
-//         return 0;
-//     }
-//     if (n === 1) {
-//         return 1;
-//     }
-//     return fibonacci(n - 1) + fibonacci(n - 2);
-// }
+function anagramTekshirish(soz1, soz2) {
+    if (soz1.length !== soz2.length) {
+      return false;
+    }
+    const belgiHisobi = {};
+    for (let i = 0; i < soz1.length; i++) {
+      const belgi = soz1[i];
+      belgiHisobi[belgi] = (belgiHisobi[belgi] || 0) + 1;
+    }
+    for (let i = 0; i < soz2.length; i++) {
+      const belgi = soz2[i];
+      if (!belgiHisobi[belgi]) {
+        return false;
+      }
+      belgiHisobi[belgi]--;
+    }
+    for (const hisob in belgiHisobi) {
+      if (belgiHisobi[hisob] !== 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  console.log(anagramTekshirish('listen', 'silent')); 
+  console.log(anagramTekshirish('listen', 'silentx')); 
+  console.log(anagramTekshirish('listen', 'silenty')); 
 
-// console.log(fibonacci(0));
-// console.log(fibonacci(2));
-// console.log(fibonacci(1));
-// console.log(fibonacci(3));
-// console.log(fibonacci(4));
-// console.log(fibonacci(5));
-// console.log(fibonacci(6));
+
+
+
+
+
+
+
+
+function fibonacci(n) {
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(0));
+console.log(fibonacci(2));
+console.log(fibonacci(1));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+console.log(fibonacci(6));
 
